7e04cd451cc94c95042635fd4ba131fdc23c8ac2
emanuelibarboza <emanuelibarbozadasilva@gmail.com>
2023-07-19 14:12:48 -0300

    CRUDcatalogo

---
+++ ASPNETMVCCRUD/Program.cs
@@ -0,0 +1,32 @@
+using ASPNETMVCCRUD.Dados;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDemoDbContext>(options => options.UseSqlServer("Data Source=CQI-BNE-844\\SQLEXPRESSMANU;Initial Catalog=Employees;Integrated Security=False;User ID=sa;Password=123456;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
